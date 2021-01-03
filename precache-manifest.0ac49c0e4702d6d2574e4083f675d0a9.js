c973f24880c8817095cbfdd66dd5812704507202
Agustina koch <agustinapkoch@gmail.com>
2021-01-03 14:46:50 -0300

    Updates

---
+++ precache-manifest.0ac49c0e4702d6d2574e4083f675d0a9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16c64559d1e45c815a5b06305af2e2b9",
+    "url": "/Project-3---Reserva-de-Alojamientos/index.html"
+  },
+  {
+    "revision": "a0abca310c4ac6d40647",
+    "url": "/Project-3---Reserva-de-Alojamientos/static/css/main.15dd6337.chunk.css"
+  },
+  {
+    "revision": "2c22bf517470d791c9b3",
+    "url": "/Project-3---Reserva-de-Alojamientos/static/js/2.64129a07.chunk.js"
+  },
+  {
+    "revision": "569e878394263cd68864b6526c81f10b",
+    "url": "/Project-3---Reserva-de-Alojamientos/static/js/2.64129a07.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a0abca310c4ac6d40647",
+    "url": "/Project-3---Reserva-de-Alojamientos/static/js/main.bf04c382.chunk.js"
+  },
+  {
+    "revision": "fb439eecd7e2979b8977",
+    "url": "/Project-3---Reserva-de-Alojamientos/static/js/runtime-main.e025e99d.js"
+  }
+]);
